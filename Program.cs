2d1711da7700076e91a44aa2b5f5933102c4803b
Otavio <otavio@otavio.com>
2019-04-15 10:28:18 -0300

    my first dotnet core hello world

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hello_world_dotnet_core_3._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}
